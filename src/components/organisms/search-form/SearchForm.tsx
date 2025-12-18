d33f7013939660497d9f519adcb8fb2a2ebc6502
sevaslavik <seva5000pro@gmail.com>
2025-12-18 23:51:37 +0200

    refactor: rewrite to using useGeoSearch

--- src/components/organisms/search-form/SearchForm.tsx
+++ src/components/organisms/search-form/SearchForm.tsx
@@ -5,24 +5,31 @@ import { Typography } from "../../atoms/typography";
 import styles from "./styles.module.css";
 import { Button } from "../../atoms/button";
 import { DropdownList } from "../../molecules/dropdown-list/DropdownList";
-import { useRef, useState } from "react";
-import type { DropdownItemProps } from "../../atoms/dropdown-item/DropdownItem";
-
-const items: DropdownItemProps[] = [
-  { label: "Paris", icon: "city", imageSrc: "https://flagcdn.com/fr.svg" },
-  { label: "London", icon: undefined, imageSrc: "https://flagcdn.com/gb.svg" },
-  { label: "Hotel Ritz", icon: "hotel" },
-  { label: "Grand Budapest Hotel", icon: "hotel" },
-];
+import { useRef } from "react";
+import { useGeoSearch } from "../../../hooks/useGeoSearch";
 
 export const SearchForm = () => {
   const inputRef = useRef<HTMLInputElement>(null);
-  const [open, setOpen] = useState<boolean>(false);
+  const { query, setQuery, searchResults, open, setOpen } = useGeoSearch();
+
   return (
     <form className={clsx(styles.form)}>
       <Typography variant="h2">Форма пошуку турів</Typography>
-      <TextInput ref={inputRef} onClick={() => setOpen(!open)} />
-      <DropdownList items={items} open={open} referenceRef={inputRef} />
+      <TextInput
+        ref={inputRef}
+        value={query}
+        onChange={(e) => {
+          setQuery(e.target.value);
+          setOpen(true);
+        }}
+        onFocus={() => setOpen(true)}
+        onBlur={() => setTimeout(() => setOpen(false), 150)}
+      />
+      <DropdownList
+        items={searchResults ?? []}
+        open={open}
+        referenceRef={inputRef}
+      />
       <Button>Знайти</Button>
     </form>
   );
