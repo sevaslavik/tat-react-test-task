6445d04a29d9927839b50813405383d57fd5ec2a
sevaslavik <seva5000pro@gmail.com>
2025-12-18 17:37:56 +0200

    refactor: create country and hotel types

---
+++ src/types/country.ts
@@ -0,0 +1,3 @@
+type Country = { id: string; name: string; flag: string };
+
+export type CountriesMap = Record<string, Country>;
---
+++ src/types/hotel.ts
@@ -0,0 +1,11 @@
+type Hotel = {
+  id: number;
+  name: string;
+  img: string;
+  cityId: number;
+  cityName: string;
+  countryId: string;
+  countryName: string;
+};
+
+export type HotelsMap = Record<string, Hotel>;
