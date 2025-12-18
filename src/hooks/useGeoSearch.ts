eb1026b011e342393d72bc2d69ffc5df5c3db061
sevaslavik <seva5000pro@gmail.com>
2025-12-18 23:48:36 +0200

    feat: create useGeoSearch hook

---
+++ src/hooks/useGeoSearch.ts
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+import { GeoService, } from "../services/info-service";
+import type { DropdownItemData } from "../entities/geo/dropdown.types";
+
+export const useGeoSearch = () => {
+  const [query, setQuery] = useState("");
+  const [searchResults, setSearchResults] = useState<DropdownItemData[]>();
+  const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    const loadCountries = async () => {
+      const countries = await GeoService.fetchCountries();
+      const dropdownItems = countries.map((c) => ({
+        id: c.id,
+        label: c.name,
+        type: "country" as const,
+        imageSrc: c.flag,
+      }));
+      setSearchResults(dropdownItems);
+    };
+    loadCountries();
+  }, []);
+
+  useEffect(() => {
+    if (!query) return;
+
+    const fetchResults = async () => {
+      const results = await GeoService.search(query);
+      setSearchResults(results);
+    };
+
+    fetchResults();
+  }, [query]);
+
+  return {
+    query,
+    setQuery,
+    searchResults,
+    open,
+    setOpen,
+  };
+};
