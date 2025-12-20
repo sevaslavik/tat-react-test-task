7b66e96ec7f82a1e0197fd5e5861adaa36f7457d
sevaslavik <seva5000pro@gmail.com>
2025-12-20 18:12:40 +0200

    feat: add hotel card molecule

---
+++ src/components/molecules/hotel-card/HotelCard.tsx
@@ -0,0 +1,54 @@
+import clsx from "clsx";
+
+import { Image } from "../../atoms/image/Image";
+import { Typography } from "../../atoms/typography/Typography";
+import styles from "./styles.module.css";
+import type { HotelWithPrices } from "../../../types/hotelWithPrices.state";
+import { formatDate } from "../../../helpers/formatDate";
+
+interface HotelCardProps {
+  hotel: HotelWithPrices;
+  className?: string;
+  countryFlag: string;
+}
+
+export const HotelCard = ({
+  hotel,
+  className,
+  countryFlag,
+}: HotelCardProps) => {
+  return (
+    <div className={clsx(styles.card, className)}>
+      <Image src={hotel.img} alt={hotel.name} />
+
+      <Typography variant="h3">{hotel.name}</Typography>
+      <div className={styles.location}>
+        {countryFlag && (
+          <img
+            src={countryFlag}
+            alt={hotel.countryName}
+            className={styles.flag}
+          />
+        )}
+        <Typography variant="bodySm">
+          {hotel.cityName}, {hotel.countryName}
+        </Typography>
+      </div>
+
+      {hotel.prices && (
+        <div className={styles.price}>
+          <Typography variant="bodySm">Старт туру :</Typography>
+          <Typography variant="bodySm">
+            {formatDate(hotel.prices[0].startDate)}
+          </Typography>
+          <Typography variant="h3">
+            {hotel.prices[0].amount} {hotel.prices[0].currency.toUpperCase()}
+          </Typography>
+        </div>
+      )}
+      <a className={styles.link} href="1234">
+        Відкрити ціну
+      </a>
+    </div>
+  );
+};
